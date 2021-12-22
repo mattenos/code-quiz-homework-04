57a7da009cc166c9e7728b260912ea2eadd67b53
unknown <mattenos4@gmail.com>
2021-12-21 23:28:07 -0500

    Created CSS, HTML, and JS files. Started working on timer and array for quiz questions.

---
+++ script.js
@@ -0,0 +1,29 @@
+// Function to start quiz on "Start Quiz" button to start timer. hide the intro. setInterval on to DOM. call out next function which will deal with the question
+
+// variabvle to crete function of question, identify as index and pass through question[]. where will question title go on dom. for loop for questions
+
+//create element of button for choices[]
+
+// > index[0]
+
+//function to display array of questions (object array). Questions will be a title(h3). loop
+
+// //move to next question
+// currentQuestionIndez++;
+
+// time = questions.length = 15
+// timerId = method(clocktick) 1000
+// identify the areas in DOM
+
+// getQuestion()
+// quizEnd()
+
+// clearInterval(timerId)
+
+// show end screen
+// show final score
+// save Highscore
+
+//save high score
+//local storage (push)
+// index.html, highscores.html, css, js (logic and questions(2))
